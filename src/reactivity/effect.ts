79d03323e32aec2852a410a0bb56f6077521af2e
allblueee <1491177973@qq.com>
2022-04-06 21:22:30 +0800

    effect reactive 依赖收集 触发依赖

---
+++ src/reactivity/effect.ts
@@ -0,0 +1,40 @@
+class ReactiveEffect {
+    private _fn: any;
+    constructor(fn) {
+        this._fn = fn;
+    }
+    run() {
+        activeEffect = this;
+        this._fn()
+    }
+}
+
+const targetMap = new Map()
+
+export function track(target, key) {
+    let depsMap = targetMap.get(target)
+    if (!depsMap) {
+        depsMap = new Map();
+        targetMap.set(target, depsMap)
+    }
+    let dep = depsMap.get(key)
+    if (!dep) {
+        dep = new Set(); 
+        depsMap.set(key, dep);
+    }
+    dep.add(activeEffect)
+
+}
+let activeEffect;
+export function effect(fn) {
+    const _effect = new ReactiveEffect(fn)
+    _effect.run();
+}
+
+export function trigger(target, key) {
+    let depsMap = targetMap.get(target)
+    let dep = depsMap.get(key)
+    for (const effect of dep) {
+        effect.run();
+    }
+}
