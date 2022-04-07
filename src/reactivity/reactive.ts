06293135600dbae28dc5cff447fd4e480f084b67
allblueee <1491177973@qq.com>
2022-04-07 18:36:07 +0800

    readonly 重构reactive部分代码

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,16 +1,13 @@
-import { track, trigger } from "./effect";
+import { mutableHandlers, readonlyHandlers } from "./baseHandlers";
 
-export function reactive (raw) {
-    return new Proxy(raw,{
-        get(target,key){
-            const res = Reflect.get(target, key);
-            track(target, key);
-            return res;
-        },
-        set(target,key,value){
-            const res = Reflect.set(target,key,value);
-            trigger(target, key);
-            return res;
-        }
-    })
+export function reactive(raw) {
+    return createActiveObject(raw, mutableHandlers)
+}
+
+export function readonly(raw) {
+    return createActiveObject(raw, readonlyHandlers)
+}
+
+function createActiveObject(raw: any, baseHandlers) {
+    return new Proxy(raw, baseHandlers);
 }
