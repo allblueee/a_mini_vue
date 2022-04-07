483c3986a450575777c7f54e20ed89b8878c60c9
allblueee <allblueee798@gmail.com>
2022-04-07 23:02:09 +0800

    嵌套 reactive 和 readonly 对象

--- src/reactivity/baseHandlers.ts
+++ src/reactivity/baseHandlers.ts
@@ -1,6 +1,6 @@
 import { isObject } from "../shared";
 import { track, trigger } from "./effect";
-import { reactive } from "./reactive";
+import { reactive, readonly } from "./reactive";
 
 const get = createGetter();
 const set = createSetter();
@@ -14,7 +14,7 @@ function createGetter(isReadonly = false) {
         const res = Reflect.get(target, key);
 
         if(isObject(res)){
-            return reactive(res)
+            return isReadonly? readonly(res): reactive(res)
         }
 
         if (!isReadonly) {
