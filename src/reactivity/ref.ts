52063574f374bb9a0b3272682bb5805fb4c0b70e
allblueee <allblueee798@gmail.com>
2022-04-15 14:35:36 +0800

    若ref是一个对象，做响应式+部分重构

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -1,11 +1,16 @@
-import { hasChanged } from "../shared";
+import { hasChanged, isObject } from "../shared";
 import { isTracking, trackEffects, triggerEffects } from "./effect";
+import { reactive } from "./reactive";
 
 class RefImpl {
     private _value: any;
     public dep;
+    private _rawvalue: any;
     constructor(value) {
-        this._value = value
+        this._rawvalue = value;
+        this._value = convert(value)
+        // value 是对象 则用 reactive 包裹
+        // 如 {count:1}是value值
         this.dep = new Set()
     }
 
@@ -16,19 +21,24 @@ class RefImpl {
     }
 
     set value(newValue) {
-        if (hasChanged(newValue, this._value)) {
-            this._value = newValue
+        if (hasChanged(newValue, this._rawvalue)) {
+            this._rawvalue = newValue
             // 触发依赖
+            this._value = convert(newValue)
             triggerEffects(this.dep)
         }
     }
+}
+
+function convert(value) {
+    return isObject(value)?reactive(value):value
 }
 
 export function ref(value) {
     return new RefImpl(value)
 }
 
-function trackRefValue(ref){
+function trackRefValue(ref) {
     if (isTracking()) {
         trackEffects(ref.dep)
     }
